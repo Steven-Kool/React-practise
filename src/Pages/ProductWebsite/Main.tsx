4ebb9598a8503fc10cdf422378135dd3749c1e5b
Steven <stevendool15@gmail.com>
2023-09-20 17:37:33 +0630

    automating the slides

--- src/Pages/ProductWebsite/Main.tsx
+++ src/Pages/ProductWebsite/Main.tsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 import Slide from "../../components/Slides";
 
 interface SlideData {
-   category: string;
+   category: {2: string}
    description: string;
-   image: string;
+   images: {0: string};
+   price: number;
    title: string;
 }
 
@@ -13,6 +14,8 @@ const ProductWebsite = () => {
    const [scrollY, setScrollY] = useState(0);
    const [slides, setSlides] = useState<SlideData[]>([]);
    const [loading, setSlideLoading] = useState(true);
+   const [leftMargin, setLeftMargin] = useState(0);
+   const [isClicked, setIsClicked] = useState(false);
 
    useEffect(() => {
       const handleScroll = () => {
@@ -35,7 +38,7 @@ const ProductWebsite = () => {
    }, [slides]);
 
    useEffect(() => {
-      fetch('https://fakestoreapi.com/products?limit=5')
+      fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=5')
          .then(response => response.json())
          .then(data => {
             console.log(data);
@@ -46,27 +49,44 @@ const ProductWebsite = () => {
          });
    }, []);
 
+   const clickOrNot = () => {
+      setIsClicked(true);
+   }
+
+   useEffect(() => {
+      const intervalId = setInterval(() => {
+         if (!isClicked) {
+            setLeftMargin(prevMargin => (prevMargin - 100 >= -400) ? prevMargin - 100 : 0)
+         }
+      }, 5000);
+
+      return () => {
+         clearInterval(intervalId);
+      };
+   }, [isClicked]);
+
    return (
       <div className="m-0" style={{padding: '100px 0 0 50px', backgroundColor: 'rgb(36, 36, 40)'}}>
          <Navbar scroll={scrollY} />
 
          <div className="w-full h-auto m-0 p-2.5 flex justify-center">
             <div className="m-0 p-0 border-0 rounded-2xl overflow-hidden" style={{width: '80%', height: '500px'}}>
-               {!loading ? (
+               {loading ? (
                   <div className="w-full h-full flex justify-center items-center" style={{ backgroundColor: 'rgb(80, 80, 80)' }}>
                      <div>
                         <img src="/src/public/22.gif" className='w-full h-full z-10'/>
                      </div>
                   </div>
                ) : (
-                  <div className='h-full flex flex-row' style={{width: '500%'}}>
+                  <div className='h-full flex flex-row' style={{width: '500%', marginLeft: `${leftMargin}%`, transition: 'margin-left 0.3s'}}>
                      {slides.map((slide, index) => (
                         <Slide
                            key={index}
-                           imgUrl={slide.image}
-                           category={slide.category}
+                           imgUrl={slide.images[0]}
+                           category={slide.category[2]}
                            productName={slide.title}
                            description={slide.description}
+                           clickOrNot={clickOrNot}
                         />
                      ))} 
                   </div>
--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -5,12 +5,13 @@ interface Props {
    category?: string;
    productName?: string;
    description?: string;
+   clickOrNot: () => void;
 }
 
-const Slide = ({ imgUrl, category, productName, description }: Props) => {
+const Slide = ({ imgUrl, category, productName, description, clickOrNot }: Props) => {
    return (
-      <div className='relative w-full h-full flex justify-end items-center' style={{backgroundColor: 'rgb(50, 50, 40)'}}>
-         <div className="absolute ml-0 h-full w-1/2 bg-transparent text-slate-200 flex flex-col justify-end items-start z-20" style={{padding: '10px 0 40px 30px'}}>
+      <div className='relative w-full h-full flex flex-row justify-end items-center' style={{backgroundColor: 'rgb(50, 50, 40)'}} onClick={clickOrNot}>
+         <div className="absolute left-0 h-full w-1/2 bg-transparent text-slate-200 flex flex-col justify-end items-start z-20" style={{padding: '10px 0 40px 30px'}}>
             <div className="text-2xl font-medium mb-2.5" style={{fontFamily: 'Inconsolata, monospace', color:' rgb(238, 165, 30)'}}># {category}</div>
             <div className="text-4xl font-semibold mb-8" style={{fontFamily: 'Poppins, sans-serif'}}>{productName}</div>
             <div className="text-xl font-medium mb-5" style={{fontFamily: 'Arial, Helvetica, sans-serif', color: 'rgb(255, 94, 120)'}}>{description}</div>
@@ -21,7 +22,7 @@ const Slide = ({ imgUrl, category, productName, description }: Props) => {
             </div>
          </div>
          
-         <div className="relative w-3/5 h-full text-white flex justify-center">
+         <div className="mr-0 relative w-3/5 h-full flex justify-center bg-white">
             <img src={imgUrl} className="w-auto h-full" />
             <div className="absolute w-full h-full z-10" style={{background: "linear-gradient(to right, rgba(50, 50, 40, 1), rgba(50, 50, 40, 0.1), rgba(50, 50, 40, 0.1))"}}></div>
          </div>
